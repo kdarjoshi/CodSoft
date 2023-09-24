6780fe2df1a0459d5291d185eca6ad0ce1e87d48
kdarjoshi <83814833+kdarjoshi@users.noreply.github.com>
2023-09-24 12:39:38 +0530

    Update todolist.cpp

--- codsoft_4/todolist.cpp
+++ codsoft_4/todolist.cpp
@@ -1,148 +1,152 @@
-#include<bits/stdc++.h>
-#include<fstream>
+#include <iostream>
+#include <fstream>  // Include the necessary header for file operations
 using namespace std;
-//structure to store the list item
-struct todolist{
+
+// Structure to store a list item
+struct todolist {
     int id;
     string task;
 };
+
 int Id;
-/* this function is the header for this application
+
+/* This function is the header for this application
    which shows the heading as "MY TODO LIST" 
 */
-void banner(){
-    cout<<"____________________________________"<<endl;
-    cout<<"\t      MY TODO LIST!"<<endl;
-    cout<<"____________________________________"<<endl;
+void banner() {
+    cout << "____________" << endl;
+    cout << "\t      MY TODO LIST!" << endl;
+    cout << "____________" << endl;
 }
 
-/* this function adds a task to the list by prompting 
+/* This function adds a task to the list by prompting 
    a message to add the required task
 */
-void addTask(){
-    system("cls");
-    cout<<"Enter the new task:"<<endl;
+void addTask() {
+    system("clear");
+    cout << "Enter the new task:" << endl;
     todolist todo;
     cin.get();
-    getline(cin,todo.task);
+    getline(cin, todo.task);
     char save;
-    cout<<"Save? (y/n):";
-    cin>>save;
-    if(save=='y'){
+    cout << "Save? (y/n):";
+    cin >> save;
+    if (save == 'y') {
         Id++;
         ofstream fout;
-        fout.open("todo.txt",ios::app);
-        fout<<"\n "<<Id;
-        fout<<"\n"<<todo.task;
+        fout.open("todo.txt", ios::app);
+        fout << "\n " << Id;
+        fout << "\n" << todo.task;
         fout.close();
         char more;
-        cout<<"Do you want to add more? (y/n):";
-        cin>>more;
-        if(more=='y'){
+        cout << "Do you want to add more? (y/n):";
+        cin >> more;
+        if (more == 'y') {
             addTask();
         }
-        else{
-            system("cls");
-            cout<<"Tasks added succesfully!"<<endl;
+        else {
+            system("clear");
+            cout << "Tasks added successfully!" << endl;
             return;
         }
     }
-    system("cls");
+    system("clear");
 }
 
-/* this funcion displays all the tasks present in the list*/
-void showTask(){
-    system("cls");
+/* This function displays all the tasks present in the list*/
+void showTask() {
+    system("clear");
     banner();
     todolist todo;
     ifstream fin;
-    fin.open("todo.txt",ios::app);
-    cout<<"Tasks:"<<endl;
-    while(!fin.eof()){
-        fin>>todo.id;
+    fin.open("todo.txt");  // Open the file for reading
+    cout << "Tasks:" << endl;
+    while (fin >> todo.id) {
         fin.ignore();
-        getline(fin,todo.task);
-        if(todo.task!=""){
-            cout<<todo.id<<": "<<todo.task<<endl;
+        getline(fin, todo.task);
+        if (todo.task != "") {
+            cout << todo.id << ": " << todo.task << endl;
         }
-        else{
-            cout<<"List Empty!"<<endl;
+        else {
+            cout << "List Empty!" << endl;
             break;
         }
     }
-        fin.close();
-        char exit;
-        cout<<"Exit? (y/n):";
-        cin>>exit;
-        if(exit!='y'){
-            showTask();
-        }
-        system("cls");
+    fin.close();  // Close the file
+    char exit;
+    cout << "Exit? (y/n):";
+    cin >> exit;
+    if (exit != 'y') {
+        showTask();
+    }
+    system("clear");
 } 
 
-/* this function deletes the required task by copying all the tasks
+/* This function deletes the required task by copying all the tasks
    to another file and neglecting the item to be deleted
 */
-void deleteTask(){
-    system("cls");
+void deleteTask() {
+    system("clear");
     int id;
-    cout<<"Enter the task id to delete: ";
-    cin>>id; 
+    cout << "Enter the task id to delete: ";
+    cin >> id; 
     char del;
-    cout<<"Delete? (y/n):";
-    cin>>del;
-    if(del=='y'){
+    cout << "Delete? (y/n):";
+    cin >> del;
+    if (del == 'y') {
         todolist todo;
         ofstream tempFile;
-        tempFile.open("temp.txt",ios::app);
+        tempFile.open("temp.txt");  // Open the temp file for writing
         ifstream fin;
-        fin.open("todo.txt");
-        int index=1;
-        while(!fin.eof()){
-            fin>>todo.id;
+        fin.open("todo.txt");  // Open the original file for reading
+        int index = 1;
+        while (fin >> todo.id) {
             fin.ignore();
-            getline(fin,todo.task);
-            if(todo.id!=id){
-                tempFile<<"\n"<<index;
-                tempFile<<"\n"<<todo.task;
+            getline(fin, todo.task);
+            if (todo.id != id) {
+                tempFile << "\n" << index;
+                tempFile << "\n" << todo.task;
                 index++;
                 Id--;
             }
         }
-        fin.close();
-        tempFile.close();
-        remove("todo.txt");
-        rename("temp.txt","todo.txt");
-        system("cls");
-        cout<<"Deleted successfully!"<<endl;
+        fin.close();  // Close the original file
+        tempFile.close();  // Close the temp file
+        remove("todo.txt");  // Remove the original file
+        rename("temp.txt", "todo.txt");  // Rename the temp file
+        system("clear");
+        cout << "Deleted successfully!" << endl;
     }
-    else{
-        system("cls");
-        cout<<"Deletion unsuccess!!"<<endl;
+    else {
+        system("clear");
+        cout << "Deletion unsuccessful!" << endl;
     }
-    
 }
-/* main function*/
-int main(){
-    int choice;
-    system("cls");
-    while(true){
-        banner();
-        cout<<"1.Add Task"<<endl;
-        cout<<"2.Show Task"<<endl;
-        cout<<"3.Delete Task"<<endl;
-        cout<<"Enter your choice"<<endl;
-        cin>>choice;
-        switch(choice){
-            case 1:addTask();
-                    break;
-            case 2:showTask();
-                    break;
-            case 3: deleteTask();
-                    break;
-            default:cout<<"invalid choice!!"<<endl;
-        }
 
+/* Main function */
+int main() {
+    int choice;
+    system("clear");
+    while (true) {
+        banner();
+        cout << "1. Add Task" << endl;
+        cout << "2. Show Task" << endl;
+        cout << "3. Delete Task" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                addTask();
+                break;
+            case 2:
+                showTask();
+                break;
+            case 3:
+                deleteTask();
+                break;
+            default:
+                cout << "Invalid choice!" << endl;
+        }
     }
     return 0;
 }
